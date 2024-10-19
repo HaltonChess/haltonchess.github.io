75a3143fe6973b2f60f63a0540868db164c6be31
VidsterBroyoMinvest <63118171+VidsterBroyo@users.noreply.github.com>
2024-10-19 01:18:44 -0400

    uh

--- static/leaderboard.js
+++ static/leaderboard.js
@@ -17,3 +17,20 @@ function updateLeaderboard(){
     alert("the winner was "+ winner+ " and the loser was "+loser)
 }
 
+async function listMajors() {
+    let response;
+    try {
+      // Fetch first 10 files
+      response = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1spa_TvGmq1HN1h5b5ICQ5gdwCMNsM3N9ztesJ8UG4sI',
+        range: 'Ladder!B2:B',
+      });
+    } catch (err) {
+      document.getElementById('content').innerText = err.message;
+      return;
+    }
+
+    console.log(response)
+    console.log(response.result.values)
+    document.getElementById('content').innerText = response;
+  }
